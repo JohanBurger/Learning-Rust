ccb66493b11fbe28aba3cabfef38a084518784fc
Johan <jdpburger@gmail.com>
2024-11-06 18:02:06 +0100

    Use clap for argument parsing

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Resources
+## Beginning
 1. [Learning Rust - No Boilerplate (YouTube)](https://www.youtube.com/watch?v=2hXNd6x9sZs)
 2. [The Rust Programming Languate (The Book)](https://doc.rust-lang.org/stable/book/) or the [Interactive Brown Version](https://rust-book.cs.brown.edu/)
 3. [Rust by Example](https://doc.rust-lang.org/rust-by-example/)
--- command_line_applications/grrs/src/main.rs
+++ command_line_applications/grrs/src/main.rs
@@ -1,3 +1,18 @@
+// use std::env::args;
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    // The pattern to look for
+    pattern: String,
+    // The path to the file to read
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
