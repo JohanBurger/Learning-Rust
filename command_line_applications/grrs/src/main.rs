8d72b55ace2f3af13039d004aec37e87d66ada17
Johan <jdpburger@gmail.com>
2024-11-15 16:05:39 +0100

    Naive implementation

--- command_line_applications/grrs/src/main.rs
+++ command_line_applications/grrs/src/main.rs
@@ -3,16 +3,23 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Args {
-    // The pattern to look for
+    /// The pattern to look for
     pattern: String,
-    // The path to the file to read
+    /// The path to the file to read
     path: PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .expect("could not read file");
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
